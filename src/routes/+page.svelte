fbb6c62b591ee957d7d7f917d26cb661ba982918
doanjus1 <justindoan86@gmail.com>
2024-07-20 11:48:23 -0400

    Learning Svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import {hello} from "./hello";
+    import type {PageData} from "./$types";
+
+    const greetingObject = {
+        greeting: "Test",
+        name: "World"
+    };
+
+    export let data: PageData;
+</script>
+
+<h1>Welcome to {hello} {greetingObject.name}</h1>
+{#each data.monsters as monster (monster.url)}
+    <p>{monster.id}: {monster.name} </p>
+{/each}
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
